48fd061897d18d7acd10d8ba1c5703cc4d9f100d
Kevin Cabrera <keviincabrera@gmail.com>
2018-12-07 13:03:36 +0000

    ~

--- prueba.js
+++ prueba.js
@@ -3,18 +3,14 @@ const mongodb = require('mongodb');
 let MongoClient = mongodb.MongoClient;
 const mongoUrl = 'mongodb://localhost:27017/BOOKS_DB';
 
-const callback = (err, db) => {
-    let collection = db.collection('books');
-    let books;
-    collection.find({}).toArray().then(data => books = data);
-    return books;
-    db.close();
-}
+function showBooks() {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
 
-const showBooks = (pathName) => {
-    return new Promise((resolve, reject) => MongoClient.connect(mongoUrl, () => {
-        console.log(callback(err,db))
-    }));
+            let collection = db.collection('books');
+            collection.find({}).toArray((err, result) => resolve(result));
+        });        
+    });
 }
 
 showBooks();
