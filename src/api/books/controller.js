6f9cf9723ed2f8e949b6d73a1a61359f098800a6
Kevin Cabrera <keviincabrera@gmail.com>
2018-12-11 23:13:55 +0000

    ~

--- src/api/books/controller.js
+++ src/api/books/controller.js
@@ -10,6 +10,31 @@ function showBooks() {
     });
 }
 
+function insertBook (book) {
+    return new Promise ((resolve, reject) => {
+        showBooks().then(data => {
+            data.push(book);
+            resolve(data);
+        });
+    });
+}
+        
+
+function saveBook(book) {
+    insertBook(book).then( data => {
+        fs.writeFile(pathName, JSON.stringify(data), err => err ? console.log(err) : console.log('Book saved!'));
+    });
+}
+
+const myBook = {
+    id: 3,
+    name: "The Meaning of General Relativity",
+    author: "Albert Einstein",
+    year: 1967
+}
+
+saveBook(myBook);
+
 module.exports = {
     showBooks
 }
